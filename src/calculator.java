bfca1100b266c9794cc019bea58efa0ce44f65c1
saemali <sabiha786!>
2021-08-18 17:19:18 +0530

    first commit

---
+++ src/calculator.java
@@ -0,0 +1,14 @@
+public class calculator {
+
+    public static void main(String[] args) {
+
+    }
+}
+class calulate{
+    int a;
+    int b;
+    
+    static void add(int a , int b){
+
+    }
+}
